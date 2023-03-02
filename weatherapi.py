a88975daa4ee9d030523b8662f132fbe8936cb42
TheTechXNinja <124846038+TheTechXNinja@users.noreply.github.com>
2023-03-01 18:54:17 -0600

    Add files via upload

---
+++ weatherapi.py
@@ -0,0 +1,30 @@
+#!/Library/Frameworks/Python.framework/Versions/3.10/bin/python3
+
+import requests
+
+API_KEY = "d52ae1ea909bc26a44789c3d8a755d20"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?lat="
+GEO_BASE_URL = "http://api.openweathermap.org/geo/1.0/direct?q="
+
+#get geo long and lat
+
+city_name = input("enter a city name: ")
+geo_request_url = f"{GEO_BASE_URL}{city_name}&limit=5&appid={API_KEY}"
+geo_response = requests.get(geo_request_url)
+geo_data = geo_response.json()
+lat = geo_data[0]["lat"]
+long = geo_data[0]["lon"]
+
+
+request_url = f"{BASE_URL}{lat}&lon={long}&appid={API_KEY}"
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data['weather'][0]['description']
+    temperature = round(data["main"]["temp"] - 273.15, 2)
+
+    print("Weather:", weather)
+    print("Temperature:", temperature, "celsius")
+else:
+    print("error:" + response)
